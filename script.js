74756ba7e6e7eb68fde45443759aa62f8be41af7
Levi <127esacramento@frhsd.com>
2026-05-01 12:53:27 -0400

    modal

---
+++ script.js
@@ -0,0 +1,4 @@
+ document.addEventListener('DOMContentLoaded', function () {
+            var instructionsModal = new bootstrap.Modal(document.getElementById('instructionsModal'));
+            instructionsModal.show();
+        });
