69f32b0ba489db303b3359a4d4b1d652a85dcdaf
Levi <127esacramento@frhsd.com>
2026-05-12 12:52:55 -0400

    array

--- script.js
+++ script.js
@@ -2,3 +2,30 @@
             var instructionsModal = new bootstrap.Modal(document.getElementById('instructionsModal'));
             instructionsModal.show();
         });
+
+
+let choices=["Rock","Paper","Scissors"];
+let codeChoice= choices[Math.floor(Math.random() * choices.length)];
+console.log(codeChoice);
+
+
+function generateCodeChoice(){
+
+}
+generateCodeChoice();
+
+
+function whoIsTheWinner(){
+
+}
+
+
+function updateScoreboard(){
+
+}
+
+
+function resetGame(){
+
+}
+
